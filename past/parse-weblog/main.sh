1cfa640c991fd78bc3c02bba24e12effa188bf11
Cheuk Lau <cheuk.lau@aggienetwork.com>
2020-07-12 11:28:52 -0700

    Fix bash regex

--- past/parse-weblog/main.sh
+++ past/parse-weblog/main.sh
@@ -8,4 +8,4 @@
 # Next we run uniq -c which gets the count of each unique IP
 # Next we sort by count in reverse order so that the greatest count is first
 # Finally we return the top 10 results
-grep -Eo "^\d{3}\.\d{3}\.\d{3}" webserver.log | sort | uniq -c | sort -r | head -10
+grep -Eo "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" webserver.log | sort | uniq -c | sort -r | head -10
